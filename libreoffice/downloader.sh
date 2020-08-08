93710c395e3f7ac55dfb176b932e0cbd5ee5d34b
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2020-08-07 21:47:41 -0400

    Add git repo prefix to paths

--- libreoffice/downloader.sh
+++ libreoffice/downloader.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y rsync;
 timestamp=$(curl "http://download.documentfoundation.org/TIMESTAMP");
 if [[ $(< TIMESTAMP) != "$timestamp" ]]; then
-    echo "$timestamp" > TIMESTAMP;
+    echo "$timestamp" > oss_rss/libreoffice/TIMESTAMP;
 	N=15;
 	(
 	while read -r url;
@@ -13,23 +13,23 @@ if [[ $(< TIMESTAMP) != "$timestamp" ]]; then
 			echo "$url"
 		fi &)
 	done < <(rsync --dry-run -avz rsync://rsync.documentfoundation.org/tdf-pub/ | awk '{print $5}' | sed -e 's/.*/http:\/\/download.documentfoundation.org\/&\.torrent/')
-	) > all_torrent_links.txt;
-	sed 's/\.torrent/\.magnet/g' all_torrent_links.txt > all_magnet_links.txt;
+	) > oss_rss/libreoffice/all_torrent_links.txt;
+	sed 's/\.torrent/\.magnet/g' oss_rss/libreoffice/all_torrent_links.txt > oss_rss/libreoffice/all_magnet_links.txt;
 
 	# html links
-	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' all_torrent_links.txt > all_torrent_links.html;
-	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' all_magnet_links.txt > all_magnet_links.html;
+	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' oss_rss/libreoffice/all_torrent_links.txt > oss_rss/libreoffice/all_torrent_links.html;
+	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' oss_rss/libreoffice/all_magnet_links.txt > oss_rss/libreoffice/all_magnet_links.html;
 
 	# all rss links
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\"><channel><title>LibreOffice links</title><link></link><description></description>" | tee all_torrent_links.rss all_magnet_links.rss > /dev/null;
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\"><channel><title>LibreOffice links</title><link></link><description></description>" | tee oss_rss/libreoffice/all_torrent_links.rss oss_rss/libreoffice/all_magnet_links.rss > /dev/null;
 
 	while read -r url; do
-		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> all_magnet_links.rss;
-	done < all_magnet_links.txt;
-	echo '</channel></rss>' >> all_magnet_links.rss;
+		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> oss_rss/libreoffice/all_magnet_links.rss;
+	done < oss_rss/libreoffice/all_magnet_links.txt;
+	echo '</channel></rss>' >> oss_rss/libreoffice/all_magnet_links.rss;
 
 	while read -r url; do
-		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> all_torrent_links.rss;
-	done < all_torrent_links.txt;
-	echo '</channel></rss>' >> all_torrent_links.rss;
+		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> oss_rss/libreoffice/all_torrent_links.rss;
+	done < oss_rss/libreoffice/all_torrent_links.txt;
+	echo '</channel></rss>' >> oss_rss/libreoffice/all_torrent_links.rss;
 fi
