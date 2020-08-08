20497d730d9752733f65069868d3acbd5b807a47
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2020-08-07 22:03:29 -0400

    Update downloader.sh

--- libreoffice/downloader.sh
+++ libreoffice/downloader.sh
@@ -3,7 +3,7 @@ echo "File listing";
 echo $(ls);
 timestamp=$(curl "http://download.documentfoundation.org/TIMESTAMP");
 if [[ $(< TIMESTAMP) != "$timestamp" ]]; then
-        echo "$timestamp" > libreoffice/TIMESTAMP;
+        echo "$timestamp" > ./libreoffice/TIMESTAMP;
 	N=15;
 	(
 	while read -r url;
@@ -14,23 +14,23 @@ if [[ $(< TIMESTAMP) != "$timestamp" ]]; then
 			echo "$url"
 		fi &)
 	done < <(rsync --dry-run -avz rsync://rsync.documentfoundation.org/tdf-pub/ | awk '{print $5}' | sed -e 's/.*/http:\/\/download.documentfoundation.org\/&\.torrent/')
-	) > libreoffice/all_torrent_links.txt;
-	sed 's/\.torrent/\.magnet/g' libreoffice/all_torrent_links.txt > libreoffice/all_magnet_links.txt;
+	) > ./libreoffice/all_torrent_links.txt;
+	sed 's/\.torrent/\.magnet/g' ./libreoffice/all_torrent_links.txt > ./libreoffice/all_magnet_links.txt;
 
 	# html links
-	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' libreoffice/all_torrent_links.txt > libreoffice/all_torrent_links.html;
-	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' libreoffice/all_magnet_links.txt > libreoffice/all_magnet_links.html;
+	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' ./libreoffice/all_torrent_links.txt > ./libreoffice/all_torrent_links.html;
+	sed 's/.*/\<a href=\"&\"\>&\<\/a\>/' ./libreoffice/all_magnet_links.txt > ./libreoffice/all_magnet_links.html;
 
 	# all rss links
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\"><channel><title>LibreOffice links</title><link></link><description></description>" | tee libreoffice/all_torrent_links.rss libreoffice/all_magnet_links.rss > /dev/null;
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\"><channel><title>LibreOffice links</title><link></link><description></description>" | tee ./libreoffice/all_torrent_links.rss ./libreoffice/all_magnet_links.rss > /dev/null;
 
 	while read -r url; do
-		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> libreoffice/all_magnet_links.rss;
-	done < libreoffice/all_magnet_links.txt;
-	echo '</channel></rss>' >> libreoffice/all_magnet_links.rss;
+		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> ./libreoffice/all_magnet_links.rss;
+	done < ./libreoffice/all_magnet_links.txt;
+	echo '</channel></rss>' >> ./libreoffice/all_magnet_links.rss;
 
 	while read -r url; do
-		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> libreoffice/all_torrent_links.rss;
-	done < libreoffice/all_torrent_links.txt;
-	echo '</channel></rss>' >> libreoffice/all_torrent_links.rss;
+		echo "$url" | sed 's/.*/\<item\>\<title\>&\<\/title\>\<link\>&\<\/link\>\<description\>\<\/description>\<\/item\>/' >> ./libreoffice/all_torrent_links.rss;
+	done < ./libreoffice/all_torrent_links.txt;
+	echo '</channel></rss>' >> ./libreoffice/all_torrent_links.rss;
 fi
